c7b5f6b3ffa3e1d4526e2e440dd066391f13b594
phace33 <phace33@gmail.com>
2019-01-24 16:11:08 +0800

    增加讀取檔案v2

--- products.py
+++ products.py
@@ -1,6 +1,15 @@
-
-
+#讀取檔案
 products = []
+with open('products.csv','r', encoding='utf-8') as f:
+    for line in f:
+        #跳過這一回
+        if '商品,價格' in line:
+            continue
+        name, price = line.strip().split(',')
+        products.append([name,price])
+print(products)
+
+#讓使用者輸入
 while True:
     name = input('請輸入商品名字')
     if name == 'q':
@@ -16,9 +25,11 @@ while True:
 
 print(products)
 
-#for p in products:
-#   print('商品',p[0],'價格是',p[1])
+#印出所有購買紀錄
+for p in products:
+    print('商品',p[0],'價格是',p[1])
 
+#寫入檔案
 with open('products.csv','w', encoding='utf-8') as f:
     f.write('商品,價格\n')
     for p in products:
