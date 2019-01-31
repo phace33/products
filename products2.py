5e587f71682617bb3c1167e1f6b6907e1348bfc1
phace33 <phace33@gmail.com>
2019-01-31 18:33:00 +0800

    程式重構

---
+++ products2.py
@@ -0,0 +1,56 @@
+import os
+#讀取檔案
+def read_file(filename):
+    products = []
+    with open(filename,'r', encoding='utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue #跳過這一回
+            name, price = line.strip().split(',')
+            products.append([name,price])
+    return products
+
+#讓使用者輸入
+def user_input(products):
+    while True:
+        name = input('請輸入商品名字')
+        if name == 'q':
+            break
+        price = input('請輸入商品價格')
+        '''完整寫法
+        p = []
+        p.append(name)
+        p.append(price)
+        '''
+        #縮寫
+        products.append([name, price])
+    print(products)
+    return products
+
+#印出所有購買紀錄
+def print_products(products):
+    for p in products:
+        print('商品',p[0],'價格是',p[1])
+
+#寫入檔案
+def write_file(filename, products):
+    with open(filename,'w', encoding='utf-8') as f:
+        f.write('商品,價格\n')
+        for p in products:
+            f.write( p[0] + ',' + str(p[1]) + '\n')
+
+def main():
+    filename = 'products.csv'
+    products = []
+    if os.path.isfile(filename): #檢查檔案在不在
+        print('找到檔案!')
+        products = read_file(filename)
+    else:
+        print('找不到檔案...')
+    products = user_input(products)
+    print_products(products)
+    write_file('products.csv', products)
+
+main()
+
+
